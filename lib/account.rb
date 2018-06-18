9e4fceddc110220db467f9ad1664a54beed34607
Mark Channer <markchanner@gmail.com>
2018-06-18 18:35:29 +0100

    Call add_transaction on statement object from within make_withdrawal method

--- lib/account.rb
+++ lib/account.rb
@@ -1,21 +1,24 @@
 class Account
   attr_reader :balance
 
-  def initialize
+  def initialize(statement)
+    @statement = statement
     @balance = 0
   end
 
   def make_deposit(amount)
     @balance += amount
+    @statement.add_transaction(amount, "", @balance)
   end
 
   def make_withdrawal(amount)
     raise 'Requested amount exceeds available funds' if @balance - amount < 0
     @balance -= amount
+    @statement.add_transaction("", amount, @balance)
   end
 
   def print_statement
-    
+
   end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,34 +2,49 @@ require 'account'
 
 describe Account do
 
+  let(:statement) { spy("statement") }
+  let(:account) { Account.new(statement) }
+
   describe '#balance' do
     it 'should return the balance' do
-      expect(subject.balance).to eq 0
+      expect(account.balance).to eq 0
     end
   end
 
   describe '#make_deposit' do
     it 'should add deposited amount to balance' do
-      subject.make_deposit(10.00)
-      expect(subject.balance).to eq 10.00
+      account.make_deposit(10.00)
+      expect(account.balance).to eq 10.00
+    end
+
+    it 'should call #add_transaction on statement' do
+      account.make_deposit(10.00)
+      account.make_deposit(10.00)
+      expect(statement).to have_received(:add_transaction).twice
     end
   end
 
   describe '#make_withdrawal' do
     it 'should deduct amount from balance' do
-      subject.make_deposit(100.00)
-      subject.make_withdrawal(90.00)
-      expect(subject.balance).to eq 10.00
+      account.make_deposit(100.00)
+      account.make_withdrawal(90.00)
+      expect(account.balance).to eq 10.00
+    end
+
+    it 'should call #add_transaction on statement' do
+      account.make_deposit(10.00)
+      account.make_withdrawal(10.00)
+      expect(statement).to have_received(:add_transaction).twice
     end
 
     it 'should not allow balance to go below zero' do
-      expect { subject.make_withdrawal(10.00) }.to raise_error("Requested amount exceeds available funds")
+      expect { account.make_withdrawal(10.00) }.to raise_error("Requested amount exceeds available funds")
     end
   end
 
   describe '#print_statement' do
     it 'should display date, credit, debit, and balance headings' do
-      expect(subject).to respond_to(:print_statement)
+      expect(account).to respond_to(:print_statement)
     end
   end
 end
