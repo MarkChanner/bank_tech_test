b41460f3915bdae6251954d3eecb3637d3a12a0c
Mark Channer <markchanner@gmail.com>
2018-06-18 14:26:17 +0100

    Add make_deposit method

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -7,4 +7,11 @@ describe Account do
       expect(subject.balance).to eq 0
     end
   end
+
+  describe '#make_deposit' do
+    it 'should add deposited amount to balance' do
+      subject.make_deposit(10)
+      expect(subject.balance).to eq 10
+    end
+  end
 end
