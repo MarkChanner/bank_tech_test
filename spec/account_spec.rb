38f5fce25f4ff335d3c822d702576efeebe0a424
Mark Channer <markchanner@gmail.com>
2018-06-18 14:50:35 +0100

    Change tested amounts from integers to doubles

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -10,21 +10,21 @@ describe Account do
 
   describe '#make_deposit' do
     it 'should add deposited amount to balance' do
-      subject.make_deposit(10)
-      expect(subject.balance).to eq 10
+      subject.make_deposit(10.00)
+      expect(subject.balance).to eq 10.00
     end
   end
 
   describe '#make_withdrawal' do
     it 'should deduct amount from balance' do
-      subject.make_deposit(100)
-      subject.make_withdrawal(90)
-      expect(subject.balance).to eq 10
+      subject.make_deposit(100.00)
+      subject.make_withdrawal(90.00)
+      expect(subject.balance).to eq 10.00
     end
 
     it 'should not allow balance to go below zero' do
       expect { subject.make_withdrawal(10) }.to raise_error("Requested amount exceeds available funds")
     end
-
   end
+
 end
